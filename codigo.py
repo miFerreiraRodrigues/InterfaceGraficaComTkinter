b87cf1734e7a5f3afaa4b59d7b2f6f00e3451efd
Mih024 <mirellarodriguesff24@gmail.com>
2026-01-19 21:31:55 -0300

    Criando a interface com a biblioteca importada

--- codigo.py
+++ codigo.py
@@ -15,6 +15,29 @@ def pegar_cotacoes():
     Euro: {cotacao_euro}
     BTC: {cotacao_btc}'''
 
-    print(texto)
+    texto_cotacoes['text'] = texto
 
-pegar_cotacoes()
+
+janela = Tk()
+janela.title('Cotação Atual das Moedas')
+janela.geometry('400x300')
+janela.config(bg="lightblue")
+
+
+# Configura a grid para expandir e centralizar
+
+janela.grid_rowconfigure(0, weight=1)
+janela.grid_rowconfigure(1, weight=1)
+janela.grid_rowconfigure(2, weight=1)
+janela.grid_columnconfigure(0, weight=1)
+
+texto_orientacao = Label(janela, text='Clique no botão para exibir as cotações das moedas')
+texto_orientacao.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+
+botao = Button(janela, text='Buscar cotações Dólar/Euro/BTC', command=pegar_cotacoes)
+botao.grid(row=1, column=0, padx=20, pady=20, sticky="nsew")
+
+texto_cotacoes = Label(janela, text='', bg="lightblue")
+texto_cotacoes.grid(row=2, column=0, padx=20, pady=20, sticky="nsew")
+
+janela.mainloop()
